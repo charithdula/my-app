e62232714f1b0dfc99c91d5bd220f32bb08025ad
Charith Jayasekara <charith@lighthole.org>
2021-09-09 19:00:11 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      MyComponentComponent,
+      CartComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
